810e3de48509afe9d8473d9535aa820916403739
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2025-02-07 00:15:01 +0200

    file committed

--- js.js
+++ js.js
@@ -160,3 +160,17 @@ function  getTheLargestPrimeNumber(primeNumbers){
  return `${max}`;
 }
 console.log(getTheLargestPrimeNumber(primeNumbers));
+// function that removes duplicate
+
+function removeDuplicates(arr) {
+    const uniqueArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (uniqueArray.includes(arr[i])) {
+            uniqueArray.push(arr[i]);
+        }
+    }
+    return uniqueArray;
+}
+const array = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArray = removeDuplicates(array);
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
