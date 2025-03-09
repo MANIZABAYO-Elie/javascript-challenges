9c3a52a6513c785ddfd1808c7843ed8e4d244141
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2025-03-09 07:49:42 +0200

    file committed

--- deep.js
+++ deep.js
@@ -1,1 +1,11 @@
 // this file contains deep explanation of javascript concept
+// Excecution context :  it contains :
+// 1.variable environment(memory);
+// 2.Thread of excution  (code)
+// How javascript code is excuted
+// ***1.memory phase.
+// **first we a have identifiers to store values in memory once
+//  they  are assigned
+ // **invoking function , the new excution content is created
+//   when invoking function the values of variables are passed 
+//   in function as an argument instead of being parameter
