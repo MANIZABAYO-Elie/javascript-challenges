0e62fc02fb9096a5a3d7e5e0940877db0b14c434
Elie MANIZABAYO <manizabayoelie.com>
2025-04-05 10:16:29 +0200

    file commited

--- my-react-app/src/header.jsx
+++ my-react-app/src/header.jsx
@@ -4,7 +4,12 @@ const Header = ()=>{
      <header>
         <h1>Welcome to online saloon</h1>
         <nav>
-            
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>contact</li>
+                <li>Services</li>
+            </ul>
         </nav>
      </header>
    );
